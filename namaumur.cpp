5e44333f09caa7b8c0aeac36ed952aa5a9cf9a07
Jeavus <akssafx1@gmail.com>
2026-02-24 16:01:11 +0700

    Penambahan namaumur.cpp

---
+++ namaumur.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+//fungsi utama
+int main() {
+    // deklarasi variabel umur dengan tipe data integer
+    int umur;
+    // memberikan nilai pada variabel umur
+    umur = 20;
+
+    // menampilkan isi string
+    cout << "Dejavu" << endl;
+    // menampilkan isi integer
+    cout << 67 << endl;
+    // menampilkan isi variabel umur ke layar
+    cout << umur << endl;
+}
